5b73b6ca38245a54ab7af014d6ccc68833ea529e
FelipeAguiar1 <felipeedu1516@gmail.com>
2022-07-24 17:32:06 -0300

    first commit

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const User = require('../models/user');
+const User = require('../app/models/user');
 const bcrypet = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-<<<<<<< HEAD
 const authConfig = require('../config/auth');
-=======
-const authConfig = require('../config/auth')
->>>>>>> 4527c8a09a498f73e07f46f161a6545fe76600ad
 
 const router = express.Router();
 
@@ -26,10 +22,7 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body);
         
         user.password = undefined;
-<<<<<<< HEAD
         
-=======
->>>>>>> 4527c8a09a498f73e07f46f161a6545fe76600ad
         return res.send({ 
             user,
             token: generateToken({ id: user.id })
@@ -51,16 +44,39 @@ router.post('/authenticate', async(req, res) => {
         return res.status(400).send({ error: 'Invalid password'});
 
     user.password = undefined;
-<<<<<<< HEAD
     
     res.send({ 
-=======
-
-    res.send({
->>>>>>> 4527c8a09a498f73e07f46f161a6545fe76600ad
         user,
         token: generateToken({ id: user.id })
     });
+})
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        
+        if (!user)
+            return res.status(400).send({ error: 'User not found' });
+            
+        const token = crypto.randomBytes(20).toString('hex');
+
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        await User.findByAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        });
+
+        console.log(token, now);
+        
+    } catch (error) {
+        res.status(400).send({ error: 'Erro on forgot password, try again!'})
+    }
 })
 
 module.exports = app => app.use('/auth', router);
--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const authMiddleware = require('../middlewares/auth');
+const authMiddleware = require('../../middlewares/auth');
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.get('/', (req, res) => {
-    res.send({ message: 'ok'});
+    res.send({ ok: true, user: req.userId });
 })
 
 module.exports = app => app.use('/projects', router);
